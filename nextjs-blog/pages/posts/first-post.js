169c3e7da3bf26ccfdb96c3903aee4c805d5a0ac
southxz <phuongnamsk109@gmail.com>
2021-04-03 22:21:23 +0700

    Pre-rendering

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,22 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
-export default function FirstPost(){
+
+export default function FirstPost() {
   return (
-    <Link href="/">
-      <a>back to home</a>
-    </Link>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First post</h1>
+      {/* <Image
+        src="/images/profile.jpg"
+        height={144}
+        width={144}
+        alt="Yourname"
+      /> */}
+    </Layout>
   )
 }
