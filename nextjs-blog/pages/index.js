c4c0901ed5a2036a1d2bc266b18e6d639d285472
southxz <phuongnamsk109@gmail.com>
2021-04-04 14:40:22 +0700

    done

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
@@ -16,17 +18,19 @@ export default function Home({ allPostsData }) {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
@@ -35,7 +39,7 @@ export default function Home({ allPostsData }) {
   )
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
     props: {
